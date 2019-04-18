058f1f43fcf819fa4daa4721eb9f302ff5a42ec4
advatrix <trojan.advatrix@gmail.com>
2019-04-18 22:12:01 +0300

    update

--- Graph/Graph/Graph/Graph.cpp
+++ Graph/Graph/Graph/Graph.cpp
@@ -62,17 +62,68 @@ struct AdjList {
 
 struct Graph {
 	AdjList* adjlist;
-	int topcount;
-	int edgecount;
+	int vertexN;
+	int edgeN;
 };
 
 const char* loadmsgs[] = { "0. Quit", "1. Load graph", "2. Generate graph", "3. Generate graph" };
 const char* menu[] = { "0. Quit", "1. Add vertex", "2. Add edge", "3. Delete vertex", "4. Decompose graph", "5. Show adjacency lists", "6. Save graph", "7. Timing", "8. Graph properties" };
 
 const int NLoadMsgs = sizeof(loadmsgs) / sizeof(loadmsgs[0]);
+const int NMenu = sizeof(menu) / sizeof(menu[0]);
+
+int dialog(const char* msgs[], int N);
+int getInt(int *t, int mode);
+int dvertexInsert(Graph*);
+int dedgeInsert(Graph*);
+int dvertexRemove(Graph*);
+int decompose(Graph*);
+int showAdjLists(Graph*);
+int dsave(Graph*);
+int timing(Graph*);
+int properties(Graph*);
+char* getStr(int mode);
+
+int(*lfptr[])(Graph*) = { NULL, dvertexInsert, dedgeInsert, dvertexRemove, decompose, showAdjLists, dsave, timing, properties };
+
+int dialog(const char* msgs[], int N) {
+	int rc;
+	do {
+		for (int i = 0; i < N; i++) puts(msgs[i]);
+		puts("Make your choice: -->");
+		if (!getInt(&rc, 0)) rc = 0;
+		if (rc >= 0 && rc < N) continue;
+		puts("I don't understant you. Try again");
+	} while (rc < 0 || rc > N);
+	return rc;
+}
+
+
+char *getStr(int mode = 1) {
+	char *ptr = (char*)malloc(sizeof(char));
+	char buf[81];
+	int n, len = 0;
+	*ptr = '\0';
+	if (mode) scanf_s("%*c");
+	do {
+		n = scanf_s("%80[^\n]", buf, 81);
+		if (n < 0) {
+			free(ptr);
+			ptr = NULL;
+			continue;
+		}
+		if (n == 0) scanf_s("%*c");
+		else {
+			len += strlen(buf);
+			ptr = (char*)realloc(ptr, len + 1);
+			strcat(ptr, buf);
+		}
+	} while (n > 0);
+	return ptr;
+}
 
 int main() {
 	Graph g = { NULL, 0, 0 };
 	int rc;
-	while (rc = dialog(loadmsgs, NLoadMsgs)) if (!lfptr[rc](&t)) break;
+	while (rc = dialog(loadmsgs, NLoadMsgs)) if (!lfptr[rc](&g)) break;
 }
